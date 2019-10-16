ee422b2795f988e84538dc0c17c4db3d61b60773
Andrew Pomeroy <andrewmichaelpomeroy@gmail.com>
2019-10-16 15:14:12 -0700

    restructure button exports

---
+++ src/components/Button/components/Button.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from '@emotion/styled/macro';
+import { borders, fontWeight } from 'styled-system';
+import { color, alignItems, justifyContent } from '@windsor/ui-kit-styled-system';
+import { buttonSizeVariant, buttonShapeVariant, buttonStyleVariant } from '../variants';
+
+const transitionDuration = 50;
+
+export const ButtonNormalized = styled.button`
+  display: inline-flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  ${alignItems}
+  ${justifyContent}
+  white-space: nowrap;
+  padding: .75em 1.5em;
+  border: 1px solid;
+  ${borders}
+  line-height: 1.25em;
+  font-weight: 500;
+  ${fontWeight}
+  font-family: inherit;
+  text-decoration: none;
+  text-transform: none;
+  background-color: transparent;
+  color: inherit;
+  user-select: none;
+  transition: border-color ${transitionDuration}ms linear,
+    background-color ${transitionDuration}ms linear,
+    transform ${transitionDuration * 2}ms ease;
+  &:focus {
+    outline: 0;
+  }
+  /* "Not-disabled" styles */
+  ${({ disabled }) =>
+  !disabled && `
+    cursor: pointer;
+    &:active {
+      transform: scale(.98);
+    }
+  `}
+  /* Disabled styles */
+  ${({ disabled }) =>
+  disabled && `
+    cursor: default;
+    opacity: .4;
+    pointer-events: none;
+  `}
+`
+
+export const ButtonThemed = styled(ButtonNormalized)`
+  ${buttonStyleVariant}
+  ${buttonSizeVariant}
+  ${buttonShapeVariant}
+  ${color}
+`
+
+ButtonThemed.defaultProps = {
+  buttonColor: 'neutral',
+  size: 'medium',
+  shape: 'soft',
+  buttonStyle: 'solid'
+}
+
+const Button = React.forwardRef(({isDisabled, children, ...props}, ref) => {
+  let disabledAttr = isDisabled ? {'disabled': 'disabled'} : {};
+  return (
+    <ButtonThemed
+      ref={ref}
+      type="button"
+      {...disabledAttr}
+      {...props}>
+      {children}
+    </ButtonThemed>)
+})
+
+Button.displayName = "Button";
+
+export default Button;
