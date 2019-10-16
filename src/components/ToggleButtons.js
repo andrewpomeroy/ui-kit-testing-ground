178430f4bf7693572c74d8d16e6ad9e3d57fa508
Andrew Pomeroy <andrewmichaelpomeroy@gmail.com>
2019-10-16 14:18:41 -0700

    add forwardRefs to toggleButtons

--- src/components/ToggleButtons.js
+++ src/components/ToggleButtons.js
@@ -6,18 +6,18 @@ import uuid4 from 'uuid4';
 
 const ToggleButtonsContext = React.createContext({});
 
-const ToggleButtons = ({children, ...props}) => {
+const ToggleButtons = React.forwardRef(({children, ...props}, ref) => {
   const [activeValue, setActiveValue] = useState(props.initialValue)
   const uuid = useRef(uuid4());
   const memoizedContext = useMemo(() => ({name: props.name || uuid.current, activeValue, setActiveValue}), [activeValue, props.name])
   return (
     <ToggleButtonsContext.Provider value={memoizedContext}>
-      <ButtonSet>
+      <ButtonSet ref={ref}>
         {children}
       </ButtonSet>
     </ToggleButtonsContext.Provider>
   );
-}
+})
 
 export default ToggleButtons;
 
@@ -31,15 +31,15 @@ HiddenRadioBtn.defaultProps = {
   autoComplete: "off"
 }
 
-export const ToggleButton = ({style, children, value, ...props}) => {
+export const ToggleButton = React.forwardRef(({style, children, value}, ref) => {
   const {name, activeValue, setActiveValue} = useContext(ToggleButtonsContext);
   const isActive = activeValue === value;
   const uuid = useRef(uuid4());
 
   return (
-    <Button as="label" style={style} buttonStyle={isActive ? 'solid' : 'hollow'} onClick={() => setActiveValue(value)}>
+    <Button ref={ref} as="label" style={style} buttonStyle={isActive ? 'solid' : 'hollow'} onClick={() => setActiveValue(value)}>
       <HiddenRadioBtn name={name} id={uuid.current} checked={isActive ? true : undefined} />
       {children}
     </Button>
   )
-}
+})
